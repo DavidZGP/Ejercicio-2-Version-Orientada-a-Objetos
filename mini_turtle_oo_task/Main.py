c28ee53ba406eab509071337aa493cd4a9916579
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-12-11 14:19:01 -0500

    Prueba Movimiento Main

--- mini_turtle_oo_task/Main.py
+++ mini_turtle_oo_task/Main.py
@@ -1,6 +1,25 @@
-from mini_turtle_oo.turtle_logic import Tortuga
+# Main.py
+from mini_turtle_oo.turtle_logic import Tortuga 
 
-t = Tortuga()
+t1 = Tortuga()
+t2 = Tortuga()
+
+print("Tortuga 1 movimientos:")
+t1.adelante(5)
+t1.abajo(2)
+t1.adelante(5)
+t1.abajo(2)
+
+print("\nTortuga 2 movimientos:")   
+t2.adelante(3)
+t2.abajo(4)
+t2.adelante(3)
+
+t1.reiniciar()
+
+print("Tortuga 1 despues de reiniciar:")
+t1.adelante(3)
+t1.abajo(3)
 
 print("Escalón 1")
 t.adelante(5)
