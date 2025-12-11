f81aa22fee9f57e2a4a9504629a89c91f4b02235
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-12-11 12:00:40 -0500

    Información Codigo 1

--- mini_turtle_oo_task/mini_turtle_oo/turtle_class.py
+++ mini_turtle_oo_task/mini_turtle_oo/turtle_class.py
@@ -1,15 +1,17 @@
-class Turtle_Class:
-    def __init__(self):
-        self.posicion_x = 0
+#Turtle_Class.py
+class Turtle_Class: # creacion de la clase Turtle_Class
+    def __init__(self):  #  constructor de la clase
+        self.posicion_x = 0  # inicializacion de la posicion en x
 
-def adelante(self, pasos):
+def adelante(self, pasos):  # metodo para mover la tortuga hacia adelante
         print("-" * pasos + ">")
         self.posicion_x += pasos
 
-def abajo(self, pasos):
+def abajo(self, pasos):  
         for _ in range(pasos):
             print(" " * self.posicion_x + "|")
         print(" " * self.posicion_x + "v")
-def reiniciar(self):
+
+def reiniciar(self):  
     self.posicion_x = 0
-    print("(Tortuga reiniciada)")
+    print("(Tortuga reiniciada)") 
