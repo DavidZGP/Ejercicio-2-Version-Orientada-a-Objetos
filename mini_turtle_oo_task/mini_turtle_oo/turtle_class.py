e2cfc5f43a989034c7f14abd4ae1255a100c1d43
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-12-11 12:01:51 -0500

    Información Codigo 2

--- mini_turtle_oo_task/mini_turtle_oo/turtle_class.py
+++ mini_turtle_oo_task/mini_turtle_oo/turtle_class.py
@@ -7,11 +7,11 @@ def adelante(self, pasos):  # metodo para mover la tortuga hacia adelante
         print("-" * pasos + ">")
         self.posicion_x += pasos
 
-def abajo(self, pasos):  
+def abajo(self, pasos):  # metodo para mover la tortuga hacia abajo
         for _ in range(pasos):
             print(" " * self.posicion_x + "|")
         print(" " * self.posicion_x + "v")
 
-def reiniciar(self):  
+def reiniciar(self):  # metodo para reiniciar la posicion de la tortuga
     self.posicion_x = 0
-    print("(Tortuga reiniciada)") 
+    print("(Tortuga reiniciada)") # mensaje de reinicio
