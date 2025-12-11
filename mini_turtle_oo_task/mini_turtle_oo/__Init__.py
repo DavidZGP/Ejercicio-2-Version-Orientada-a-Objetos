72821baf076dd5941d6e1e4c141c505de3743399
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-12-11 12:06:48 -0500

    Información Codigo Init

--- mini_turtle_oo_task/mini_turtle_oo/__Init__.py
+++ mini_turtle_oo_task/mini_turtle_oo/__Init__.py
@@ -1,5 +1,5 @@
 #__Init
 
-from turtle_class import Turtle_Class  
+from turtle_class import Turtle_Class  # importacion de la clase Turtle_Class
 
-__all__ = ["Turtle_Class"]
+__all__ = ["Turtle_Class"]  # definicion de los elementos exportables del modulo
